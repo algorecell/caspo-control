5f02cbe7c4c3f174c3e913df0e2ef3faa1596668
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-11-28 17:55:55 +0100

    first reprogramming_to_attractor implementation

--- caspo_control/__init__.py
+++ caspo_control/__init__.py
@@ -1,3 +1,23 @@
 
 __version__ = "0.1"
 
+from .iface import caspo_control
+
+from colomoto.types import PartialState
+from algorecell_types import *
+
+def reprogramming_to_attractor(bn, *goal_spec, fixed={},
+        exclude_goal=False, maxsize=0, **goal_kwspec):
+    """
+    TODO
+    """
+    goal = PartialState(*goal_spec, **goal_kwspec)
+    interventions = caspo_control(bn, goal, fixed,
+            allow_goal_intervention=not exclude_goal,
+            maxsize=maxsize)
+    strategies = ReprogrammingStrategies()
+    for control in interventions:
+        st = FromAny(PermanentPerturbation(control))
+        strategies.add(st)
+    return strategies
+
