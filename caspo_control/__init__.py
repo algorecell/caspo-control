5cfd960c5977f0ed4d694ac2c114609aabc3cf5f
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-04-06 15:58:35 +0200

    use BooleanNetwork.auto_cast for simpler input

--- caspo_control/__init__.py
+++ caspo_control/__init__.py
@@ -6,11 +6,14 @@ from .iface import caspo_control
 from colomoto.types import PartialState
 from algorecell_types import *
 
+from colomoto.minibn import BooleanNetwork
+
 def reprogramming_to_attractor(bn, *goal_spec, fixed={},
         exclude_goal=False, maxsize=0, **goal_kwspec):
     """
     TODO
     """
+    bn = BooleanNetwork.auto_cast(bn)
     goal = PartialState(*goal_spec, **goal_kwspec)
     interventions = caspo_control(bn, goal, fixed,
             allow_goal_intervention=not exclude_goal,
