643780338276b5af9920c9abcbe93ee4965d74d7
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-05-20 12:17:55 +0200

    move fixed to constructor

--- caspo_control/__init__.py
+++ caspo_control/__init__.py
@@ -8,19 +8,26 @@ from algorecell_types import *
 
 from colomoto.minibn import BooleanNetwork
 
-def reprogramming_to_attractor(bn, *goal_spec, fixed={},
-        exclude_goal=False, maxsize=0, **goal_kwspec):
-    """
-    TODO
-    """
-    bn = BooleanNetwork.auto_cast(bn)
-    goal = PartialState(*goal_spec, **goal_kwspec)
-    interventions = caspo_control(bn, goal, fixed,
-            allow_goal_intervention=not exclude_goal,
-            maxsize=maxsize)
-    strategies = ReprogrammingStrategies()
-    for control in interventions:
-        st = FromAny(PermanentPerturbation(control))
-        strategies.add(st)
-    return strategies
+class CaspoControl(object):
+    def __init__(self, bn, fixed={}):
+        self.bn = BooleanNetwork.auto_cast(bn)
+        self.fixed = fixed
+
+    def reprogramming_to_attractor(self, *goal_spec,
+            exclude_goal=False, maxsize=0, **goal_kwspec):
+        """
+        TODO
+        """
+        goal = PartialState(*goal_spec, **goal_kwspec)
+        interventions = caspo_control(self.bn, goal, self.fixed,
+                allow_goal_intervention=not exclude_goal,
+                maxsize=maxsize)
+        strategies = ReprogrammingStrategies()
+        for control in interventions:
+            st = FromAny(PermanentPerturbation(control))
+            strategies.add(st)
+        return strategies
+
+def load(bn):
+    return CaspoControl(bn)
 
