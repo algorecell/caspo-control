c8cd8d75446cec91f332d4f903a3c0f71f676994
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-06-26 23:26:18 +0200

    API documentation

--- caspo_control/__init__.py
+++ caspo_control/__init__.py
@@ -1,5 +1,36 @@
 """
-TODO
+This Python module provides a simple wrapper for the *control* method of the
+tool `caspo <https://bioasp.github.io/caspo/>`_, which implements the
+computation of intervention strategies in logical signaling networks with
+Answer-Set Programming (https://doi.org/10.1017/S1471068413000422).
+
+The control predictions can be processed using the `algorecell_types
+<https://github.com/algorecell/algorecell_types>`_ library, which eases the
+display and comparison with other control methods.
+
+Installation instructions at https://github.com/algorecell/caspo-control.
+
+Examples can be found at:
+    - https://nbviewer.jupyter.org/github/algorecell/caspo-control/tree/master/examples/
+
+Quick usage:
+
+>>> import caspo_control
+
+Model loading:
+
+>>> cc = caspo_control.load("model.bnet") # in BoolNet format
+# alternatively, load with biolqm in any format
+>>> lm = biolqm.load("model.zginml") # or any format support by bioLQM
+>>> cc = caspo_control.load(lm)
+
+Reprogramming predictions:
+
+>>> rs = cc.reprogramming_to_attractor({"A": 1, "B": 0})
+>>> rs
+
+See ``help(rs)`` for other display methods
+
 """
 
 from .iface import caspo_control
@@ -11,16 +42,37 @@ from colomoto.minibn import BooleanNetwork
 
 class CaspoControl(object):
     """
-    TODO
+    Wrapper for the `caspo control` method.
     """
     def __init__(self, bn, fixed={}):
+        """
+        :param bn: Boolean network in any format supported by
+            ``colomoto.minibn.BooleanNetwork``, which includes filename in BoolNet
+            format, and ``biolqm`` or ``ginsim`` objects.
+        :keyword dict[str,int] fixed: fix the given nodes to their associated given values
+        """
         self.bn = BooleanNetwork.auto_cast(bn)
         self.fixed = fixed
 
     def reprogramming_to_attractor(self, *goal_spec,
             exclude_goal=False, maxsize=0, **goal_kwspec):
         """
-        TODO
+        Compute reprogramming strategies ensuring that all the attractors of the
+        perturbed network match with the given specification with the
+        (general) asynchronous mode.
+        The perturbations are permanent, and can change the attractors of the
+        input model.
+
+        :keyword bool exclude_goal: Whenever ``True``, skip solutions
+            controlling the nodes specified for the reprogramming goal (default: ``False``)
+        :keyword int maxsize: maximum number of simultaneous perturbations
+            (default: ``0``, unlimited)
+        :rtype: `algorecell_types.ReprogrammingStrategies <https://algorecell-types.readthedocs.io/#algorecell_types.ReprogrammingStrategies>`_
+
+        Examples:
+
+        >>> rs = cc.reprogramming_to_attractor(A=1, B=0)
+        >>> rs = cc.reprogramming_to_attractor({"A": 1, "B": 0})
         """
         goal = PartialState(*goal_spec, **goal_kwspec)
         interventions = caspo_control(self.bn, goal, self.fixed,
@@ -37,7 +89,7 @@ class CaspoControl(object):
 
 def load(bn, fixed={}):
     """
-    TODO
+    Returns :py:class:`.CaspoControl` `(bn, fixed)` object
     """
     return CaspoControl(bn, fixed)
 
